d4022de71d5a82383c58a7f90dde49c83f030c06
Ayhann <ayhann.deniz@viacesi.fr>
2017-12-07 13:30:24 +0100

    semi responsive

--- views/header.php
+++ views/header.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8" />
     <title>Dog-Dating</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="<?php echo BASE_URL?>assets/css/font-awesome.min.css" />
