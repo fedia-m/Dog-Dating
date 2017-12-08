5bf3a107e9561f1885578f3594a48756ec7977e3
Ayhann <ayhann.deniz@viacesi.fr>
2017-12-08 14:08:56 +0100

    chien

---
+++ classes/Chien.php
@@ -0,0 +1,73 @@
+<?php 
+
+
+class Chien
+{
+	private $id;	
+	private $nom;	
+	private $race;	
+	private $naissance;	
+	private $lof;	
+	private $loof;	
+	private $numeroPuce;	
+	private $photo;	
+	private $idProprio;	
+
+	public function getId()
+	{
+		return $this->id;
+	}
+
+	public function getNom()
+	{
+		return $this->nom;
+	}
+
+	public function getRace()
+	{
+		return $this->race;
+	}
+
+	public function getNaissance()
+	{
+		return $this->naissance;
+	}
+
+	public function getLof()
+	{
+		return $this->lof;
+	}
+
+	public function getLoof()
+	{
+		return $this->loof;
+	}
+
+	public function getNumeroPuce()
+	{
+		return $this->numeroPuce;
+	}
+
+	public function getPhoto()
+	{
+		return $this->photo;
+	}
+
+	public function getIdProprio()
+	{
+		return $this->idProprio;
+	}
+
+}
+
+
+/**
+* 
+*/
+class Chiens
+{
+	
+	
+}
+
+?>
