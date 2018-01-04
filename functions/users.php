78720e160e3f484dd8dd5c8728503a694e15be85
Ayhann <ayhann.deniz@viacesi.fr>
2018-01-04 22:24:01 +0100

    connexion + mdp oublier a definir ?

--- functions/users.php
+++ functions/users.php
@@ -1,7 +1,30 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: mahatehotia
- * Date: 04/01/18
- * Time: 16:24
- */
+require_once '../config.php';
+require_once '../connexion_bdd.php';
+require_once '../classes/adherent.php';
+
+
+//Se déclenche quand l'user se connecte
+if (isset($_POST['btnConnexion'])) {
+	
+$login = $_POST['login'];
+$password = sha1(sha1($_POST['motDePasse']));
+var_dump($login);
+var_dump($password);
+
+
+$stmt = $bdd->prepare('SELECT * FROM adherents WHERE pseudoAdherent = :pseudo and mdpAdherent = :mdp');
+
+$stmt->execute(array('pseudo' => $login, 'mdp' => $password));
+
+//L'utilisateur est reconnu
+foreach ($stmt as $row) {
+    $_SESSION['utilisateur'] = new Adherent();
+    $_SESSION['utilisateur']->objetSetId($row['idAdherent'],$row['nomAdherent'],$row['prenomAdherent'],$row['pseudoAdherent'],$row['mdpAdherent'],$row['mailAdherent'],$row['adresseAdherent'],$row['sexeAdherent'],$row['avatarAdherent'],$row['roleAdherent'],$row['id_Ville']);
+  	header('Location: ../index.php');
+  	exit();
+  	break;
+}
+
+//fin du déclenchement bouton connexion user
+}
--- views/connexion.php
+++ views/connexion.php
@@ -9,13 +9,13 @@ include("menu.php");
 <div class="container-fluid">
     <div class="col-xl-6 col-lg-10 mx-auto">
         <div class="card card-body bg-light">
-            <form action="#" method="post" class="probootstrap-form mb-5">
+            <form action="../functions/users.php" method="post" class="probootstrap-form mb-5">
                 <legend class="text-center"><h1>Connexion</h1></legend>
                 <div class="row">
                     <div class="col-md-12">
                         <div class="form-group">
                             <label for="login">Login</label>
-                            <input type="text" class="form-control" id="login" name="login">
+                            <input type="text" class="form-control" id="login" name="login" required="">
                         </div>
                     </div>
                 </div>
@@ -23,10 +23,11 @@ include("menu.php");
                     <div class="col-md-12">
                         <div class="form-group">
                             <label for="motDePasse">Mot de passe</label>
-                            <input type="password" class="form-control" id="motDePasse" name="motDePasse">
+                            <input type="password" class="form-control" id="motDePasse" name="motDePasse" required="">
                         </div>
                     </div>
                 </div>
+                <p><i><a href="#">Mot de passe oublié ?</a></i></p>
                 <!-- Button -->
                 <!-- Form actions -->
                 <div class="form-group">
