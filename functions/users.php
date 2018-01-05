17ba1e13e0b7f0aa3d209729d7a2164de49f695b
Ayhann <ayhann.deniz@viacesi.fr>
2018-01-05 16:37:23 +0100

    Page profil fonctionnel, manque le menu a faire

--- functions/users.php
+++ functions/users.php
@@ -23,8 +23,8 @@ $sql->execute(array('pseudo' => $login, 'mdp' => $password));
 foreach ($sql as $row) {
     $_SESSION['utilisateur'] = new Adherent();
     $_SESSION['utilisateur']->objetSetId($row['idAdherent'],$row['nomAdherent'],$row['prenomAdherent'],$row['pseudoAdherent'],$row['mdpAdherent'],$row['mailAdherent'],$row['adresseAdherent'],$row['sexeAdherent'],$row['avatarAdherent'],$row['roleAdherent'],$row['id_Ville']);
-  	//header('Location: ../index.php');
-  	var_dump($_SESSION['utilisateur']);
+  	header('Location: ../index.php');
+  	//var_dump($_SESSION['utilisateur']);
   	exit();
   	break;
 }
