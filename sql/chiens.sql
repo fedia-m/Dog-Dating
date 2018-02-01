ada01531987959bf26ab4165cb1095928cdad5b6
mahatehotia <mahatehotia@hotmail.com>
2018-02-01 09:19:13 +0100

    ajout des samples chiens

---
+++ sql/chiens.sql
@@ -0,0 +1,7 @@
+INSERT INTO `chiens` (`idChien`, `nomChien`, `sexeChien`, `dateNaissanceChien`, `lofChien`, `numeroPuce`, `photoChien`, `descriptionChien`, `id_Adherent`, `id_Race`) VALUES
+(1, 'Rex', 'M', '2018-10-11', '2015049585-2016-1', '250261055101789', 'rex.jpg', 'Chien très joyeux', 2, 20),
+(2, 'Roxy', 'F', '2017-11-11', '2015049543-2016-1', '250261055101703', 'roxy.jpg', 'Chienne heureuse et attachante', 2, 10),
+(3, 'Tofou', 'F', '2017-11-11', '2015049543-2015-1', '250269055101703', 'tofou.jpg', 'Mâle joyeux', 2, 3),
+(4, 'Rex', 'M', '2018-10-11', '2015049585-2016-1', '250261055101789', 'rex.jpg', 'Chien très joyeux', 1, 20),
+(5, 'Roxy', 'F', '2017-11-11', '2015049543-2016-1', '250261055101703', 'roxy.jpg', 'Chienne heureuse et attachante', 1, 10),
+(6, 'Tofou', 'F', '2017-11-11', '2015049543-2015-1', '250269055101703', 'tofou.jpg', 'Mâle joyeux', 1, 3);
