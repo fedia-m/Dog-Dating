3f3f700933f85d22e443d8216bc41a029be3368c
mahatehotia <mahatehotia@hotmail.com>
2018-03-02 14:40:24 +0100

    modif data

--- sql/messages.sql
+++ sql/messages.sql
@@ -1,8 +1,8 @@
-INSERT INTO `messages` (`idMessage`, `idExpediteur`, `idDestinataire`, `idChien`, `objetMessage`, `contenuMessage`) VALUES
-  (1,1,2,1,'Info sur Rex','Bonjour j''aimerais avoir un chiot et la race de Rex m''intéresse Pouvez vous me contacter? Merci Ayhann'),
-  (2,2,1,6,'Info sur Tofou','Bonjour j''aimerais avoir un chiot et la race de Tofou m''intéresse Pouvez vous me contacter? Merci Tiona'),
-  (3,2,1,4,'Info sur Rex','Bonjour j''aimerais avoir un chiot et la race de Rex m''intéresse Pouvez vous me contacter? Merci Ayhann'),
-  (4,2,1,5,'Info sur Roxy','Bonjour j''aimerais avoir un chiot et la race de Roxy m''intéresse Pouvez vous me contacter? Merci Tiona')
+INSERT INTO `messages` (`idMessage`, `idExpediteur`, `idDestinataire`, `idChien`, `objetMessage`, `contenuMessage`,`messageArchiveE`,`messageArchiveD`) VALUES
+  (1,1,2,1,'Info sur Rex','Bonjour j''aimerais avoir un chiot et la race de Rex m''intéresse Pouvez vous me contacter? Merci Ayhann','0','0'),
+  (2,2,1,6,'Info sur Tofou','Bonjour j''aimerais avoir un chiot et la race de Tofou m''intéresse Pouvez vous me contacter? Merci Tiona','0','1'),
+  (3,2,1,4,'Info sur Rex','Bonjour j''aimerais avoir un chiot et la race de Rex m''intéresse Pouvez vous me contacter? Merci Ayhann','1','0'),
+  (4,2,1,5,'Info sur Roxy','Bonjour j''aimerais avoir un chiot et la race de Roxy m''intéresse Pouvez vous me contacter? Merci Tiona','0','0')
 
 
 
