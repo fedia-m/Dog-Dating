721c61f7b02710be35a7dd6d27f103feb6fbb81f
mahatehotia <mahatehotia@hotmail.com>
2018-03-02 14:36:42 +0100

    modif data chiens et messages

--- sql/messages.sql
+++ sql/messages.sql
@@ -1,6 +1,8 @@
 INSERT INTO `messages` (`idMessage`, `idExpediteur`, `idDestinataire`, `idChien`, `objetMessage`, `contenuMessage`) VALUES
   (1,1,2,1,'Info sur Rex','Bonjour j''aimerais avoir un chiot et la race de Rex m''intéresse Pouvez vous me contacter? Merci Ayhann'),
-  (2,2,1,5,'Info sur Roxy','Bonjour j''aimerais avoir un chiot et la race de Roxy m''intéresse Pouvez vous me contacter? Merci Tiona')
+  (2,2,1,6,'Info sur Tofou','Bonjour j''aimerais avoir un chiot et la race de Tofou m''intéresse Pouvez vous me contacter? Merci Tiona'),
+  (3,2,1,4,'Info sur Rex','Bonjour j''aimerais avoir un chiot et la race de Rex m''intéresse Pouvez vous me contacter? Merci Ayhann'),
+  (4,2,1,5,'Info sur Roxy','Bonjour j''aimerais avoir un chiot et la race de Roxy m''intéresse Pouvez vous me contacter? Merci Tiona')
 
 
 
