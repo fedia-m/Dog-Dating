efcd550c6dad388d9e15c2b33e553ffe40e561fa
mahatehotia <mahatehotia@hotmail.com>
2018-04-25 15:37:19 +0200

    edit profil répare

--- views/edit_profil.php
+++ views/edit_profil.php
@@ -13,174 +13,151 @@ require("menu.php");
 $oVilles = Villes::getInstance($bdd);
 $oCollVille = $oVilles->getCollection();
 //L'utilisateur n'as pas de chien pour l'instant, utile pour afficher ses chiens plutard
-var_dump($oVilles);
+//var_dump($oVilles);
 
 ?>
-<div class="probootstrap-bar">
-    <a href="#" class="probootstrap-toggle js-probootstrap-toggle"><span class="oi oi-menu"></span></a>
-</div>
-<div class="col-xl-8 col-lg-12 mx-auto">
-    <div class="card card-body bg-light">
-        <form action="<?php echo BASE_URL?>controllers/modifier_adherent.php" method="post" class="probootstrap-form mb-5">
-            <legend class="text-center"><h1>Edition profil</h1></legend>
-            <div class="row">
-                <div class="col-md-6">
-                    <div class="form-group">
-                        <label for="prenomUser">Prénom</label>
-                        <input type="text" class="form-control" id="prenomUser" name="prenomUser" placeholder="<?php echo $_SESSION['utilisateur']->getPrenom(); ?>">
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="form-group">
-                        <label for="nomUser">Nom</label>
-                        <input type="text" class="form-control" id="nomUser" name="nomUser" placeholder="<?php echo $_SESSION['utilisateur']->getNom(); ?>">
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-md-4">
-                    <div class="form-group">
-                        <label for="pseudo">Pseudo</label>
-                        <input type="text" class="form-control" id="pseudo" name="pseudo" placeholder="<?php echo $_SESSION['utilisateur']->getPseudo(); ?>">
-                    </div>
-                </div>
-                <div class="col-md-8">
-                    <div class="form-group">
-                        <label for="email">Email</label>
-                        <input type="email" class="form-control" id="email" name="email" placeholder="<?php echo $_SESSION['utilisateur']->getMail(); ?>">
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="form-group">
-                        <p><a href="#" onclick="modifierMdp();return false;"><span class="icon-lock">Modifier mot de passe</span></a></p>
-                    </div>
-                </div>
-            </div>
-            <div id="mdp" hidden>
-                <div class="row">
-                    <div class="col-md-12">
-                        <div class="form-group">
-                            <label for="motDePasse">Ancien mot de passe</label>
-                            <input type="password" class="form-control" id="AncienMotDePasse" name="AncienMotDePasse">
-                        </div>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-md-12">
-                        <div class="form-group">
-                            <label for="motDePasse">Nouveau mot de passe</label>
-                            <input type="password" class="form-control" id="motDePasse" name="motDePasse">
-                        </div>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-md-12">
-                        <div class="form-group">
-                            <label for="motDePasse">Confirmation</label>
-                            <input type="password" class="form-control" id="motDePasse2" name="motDePasse2">
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!-- sexe input-->
-            <div class="row">
-                <div class="col-md-6">
-                    <label for="sexe">Votre sexe</label>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-md-4">
-                    <div class="form-group">
-                        <label class="radio-inline"><input type="radio" id="homme" name="sexe" value="homme" <?php if ($_SESSION['utilisateur']->getSexe()=='H') echo "checked" ; ?> > Homme</label>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="form-group">
-                        <label class="radio-inline"><input type="radio" id="femme" name="sexe"  value="femme" <?php if ($_SESSION['utilisateur']->getSexe()=='F') echo "checked" ; ?>> Femme</label>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="form-group">
-                        <label class="radio-inline"><input type="radio" id="autre" name="sexe"  value="autre" <?php if ($_SESSION['utilisateur']->getSexe()=='A') echo "checked" ; ?>> Autre</label>
-                    </div>
-                </div>
-            </div>
-            <!--Adresse adherent -->
-            <div class="row">
-                <div class="col-md-3">
-                    <div class="form-group">
-                        <label for="sexe">Votre adresse</label>
-                    </div>
-                </div>
-             </div>
-            <div class="row">
-                <div class="col-md-9">
-                    <div class="form-group">
-                        <input type="text" class="form-control" id="adresse" name="adresse" placeholder="<?php echo $_SESSION['utilisateur']->getAdresse(); ?>">
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-md-5">
-                    <div class="form-group">
-                        <input type="text" class="form-control" id="cp" name="cp" placeholder="<?php echo $maVille->getCp(); ?>">
-                    </div>
-                </div>
-                <div class="col-md-7">
-                    <form name="a">
-                        <fieldset style="width: 500px">
-                            <legend>Liste liées</legend>
-                            <label>Code Postal</label>
-                            <input type="text" name='codePostal' id='codePostal' onchange='go()'/>
-                            <label>Ville</label>
-                            <div id='ville' style='display:inline'>
-                                <select name='ville'>
-                                    <option value='-1'>Saisissez le code postal</option>
-                                </select>
-                            </div>
-                            <?php
-                            echo '<select id="ville">';
-                                if(isset($_POST["codePostal"])){
-                                $sql = $bdd -> prepare("SELECT * FROM sg_ville WHERE codePostal=".$_POST["codePostal"]." ORDER BY ville");
-                                echo '<option value="-1">Choisissez la ville</option>';
-                                foreach ($sql as $row){
-                                echo '<option value="'.$row["idVille"].'">'.$row["ville"].'</option>';
-                                }
-                                }
-                                echo '</select>';?>
-                        </fieldset>
-                    </form>
-                    <div class="form-group">
-                        <input type="text" class="form-control" id="ville" name="ville" placeholder="<?php echo $maVille->getNom(); ?>">
-                        <input type="hidden" name="idVille" value="<?php echo $maVille->getId(); ?>">
-                    </div>
-                </div>
-            </div>
-            <!--Avatar adherent -->
-            <div class="row">
-                <div class="col-md-3">
-                    <div class="form-group">
-                        <label for="sexe">Votre photo</label>
-                    </div>
-                </div>
-                <div class="col-md-9">
-                    <div class="form-group">
-                        <input type="file">
-                    </div>
-                </div>
-            </div>
-            <!-- Form actions -->
-            <div class="form-group">
-                <div class="col-md-12 text-center">
-                    <button type="submit" name='editUser' class="btn btn-primary btn-lg">Modifier</button>
-                    <a href="<?php echo BASE_URL.'views/page_profil.php'?>"><button type="button" name='annulerCreerUser' class="btn btn-danger btn-lg">Annuler</button></a>
-                </div>
-            </div>
-        </form>
+    <div class="probootstrap-bar">
+        <a href="#" class="probootstrap-toggle js-probootstrap-toggle"><span class="oi oi-menu"></span></a>
     </div>
-</div>
+    <div class="col-xl-8 col-lg-12 mx-auto">
+        <div class="card card-body bg-light">
+            <form action="<?php echo BASE_URL?>controllers/modifier_adherent.php" method="post" class="probootstrap-form mb-5">
+                <legend class="text-center"><h1>Edition profil</h1></legend>
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label for="prenomUser">Prénom</label>
+                            <input type="text" class="form-control" id="prenomUser" name="prenomUser" placeholder="<?php echo $_SESSION['utilisateur']->getPrenom(); ?>">
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label for="nomUser">Nom</label>
+                            <input type="text" class="form-control" id="nomUser" name="nomUser" placeholder="<?php echo $_SESSION['utilisateur']->getNom(); ?>">
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            <label for="pseudo">Pseudo</label>
+                            <input type="text" class="form-control" id="pseudo" name="pseudo" placeholder="<?php echo $_SESSION['utilisateur']->getPseudo(); ?>">
+                        </div>
+                    </div>
+                    <div class="col-md-8">
+                        <div class="form-group">
+                            <label for="email">Email</label>
+                            <input type="email" class="form-control" id="email" name="email" placeholder="<?php echo $_SESSION['utilisateur']->getMail(); ?>">
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="form-group">
+                            <p><a href="#" onclick="modifierMdp();return false;"><span class="icon-lock">Modifier mot de passe</span></a></p>
+                        </div>
+                    </div>
+                </div>
+                <div id="mdp" hidden>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="form-group">
+                                <label for="motDePasse">Ancien mot de passe</label>
+                                <input type="password" class="form-control" id="AncienMotDePasse" name="AncienMotDePasse">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="form-group">
+                                <label for="motDePasse">Nouveau mot de passe</label>
+                                <input type="password" class="form-control" id="motDePasse" name="motDePasse">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="form-group">
+                                <label for="motDePasse">Confirmation</label>
+                                <input type="password" class="form-control" id="motDePasse2" name="motDePasse2">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- sexe input-->
+                <div class="row">
+                    <div class="col-md-6">
+                        <label for="sexe">Votre sexe</label>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            <label class="radio-inline"><input type="radio" id="homme" name="sexe" value="homme" <?php if ($_SESSION['utilisateur']->getSexe()=='H') echo "checked" ; ?> > Homme</label>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            <label class="radio-inline"><input type="radio" id="femme" name="sexe"  value="femme" <?php if ($_SESSION['utilisateur']->getSexe()=='F') echo "checked" ; ?>> Femme</label>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            <label class="radio-inline"><input type="radio" id="autre" name="sexe"  value="autre" <?php if ($_SESSION['utilisateur']->getSexe()=='A') echo "checked" ; ?>> Autre</label>
+                        </div>
+                    </div>
+                </div>
+                <!--Adresse adherent -->
+                <div class="row">
+                    <div class="col-md-3">
+                        <div class="form-group">
+                            <label for="sexe">Votre adresse</label>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-9">
+                        <div class="form-group">
+                            <input type="text" class="form-control" id="adresse" name="adresse" placeholder="<?php echo $_SESSION['utilisateur']->getAdresse(); ?>">
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-5">
+                        <div class="form-group">
+                            <input type="text" class="form-control" id="cp" name="cp" placeholder="<?php echo $maVille->getCp(); ?>">
+                        </div>
+                    </div>
+                    <div class="col-md-7">
+                        <div class="form-group">
+                            <input type="text" class="form-control" id="ville" name="ville" placeholder="<?php echo $maVille->getNom(); ?>">
+                            <input type="hidden" name="idVille" value="<?php echo $maVille->getId(); ?>">
+                        </div>
+                    </div>
+                </div>
+                <!--Avatar adherent -->
+                <div class="row">
+                    <div class="col-md-3">
+                        <div class="form-group">
+                            <label for="sexe">Votre photo</label>
+                        </div>
+                    </div>
+                    <div class="col-md-9">
+                        <div class="form-group">
+                            <input type="file">
+                        </div>
+                    </div>
+                </div>
+                <!-- Form actions -->
+                <div class="form-group">
+                    <div class="col-md-12 text-center">
+                        <button type="submit" name='editUser' class="btn btn-primary btn-lg">Modifier</button>
+                        <a href="<?php echo BASE_URL.'views/page_profil.php'?>"><button type="button" name='annulerCreerUser' class="btn btn-danger btn-lg">Annuler</button></a>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
 <?php
 require_once 'footer.php';
 ?>
--- views/footer.php
+++ views/footer.php
@@ -1,7 +1,7 @@
 
     </main>
     </div>
-    <script src="<?php echo BASE_URL?>assets/js/jquery-3.2.1.slim.min.js"></script>
+    <script src="<?php echo BASE_URL?>assets/js/jquery-3.2.1.min.js"></script>
     <script src="<?php echo BASE_URL?>assets/js/popper.min.js"></script>
     <script src="<?php echo BASE_URL?>assets/js/bootstrap.min.js"></script>
     <script src="<?php echo BASE_URL?>assets/js/owl.carousel.min.js"></script>
@@ -34,5 +34,67 @@
         }
     </script>
 
+    <script>
+        jQuery(function ($) {
+            var _code = $('#code'), _city = $('#city'), _out = $('#output');
+            var _cache = {};
+            var _host = (~(location.protocol + '').indexOf('s')
+                        ? 'https'
+                        : 'http'
+                ) + '://vicopo.selfbuild.fr';
+            _code.keyup(function () {
+                var _val = $(this).val();
+                if(/^[^0-9]/.test(_val)) {
+                    _code.val('');
+                    _city.val(_val).focus().trigger('keyup');
+                }
+            });
+            _city.keyup(function () {
+                var _val = $(this).val();
+                if(/^[0-9]/.test(_val)) {
+                    _city.val('');
+                    _code.val(_val).focus().trigger('keyup');
+                }
+            });
+            function _done(_cities) {
+                _out.html(_cities.map(function (_data) {
+                    return '<a href="#">' + _data.code + ' &nbsp; ' + _data.city + '</a>';
+                }).join(''));
+            }
+            $.each(['code', 'city'], function (i, _name) {
+                var _input = $('#' + _name).on('keyup', function () {
+                    var _val = _input.val();
+                    if(_val.length > 1) {
+                        _cache[_name] = _cache[_name] || {};
+                        if(_cache[_name][_val]) {
+                            _done(_cache[_name][_val]);
+                        }
+                        var _data = {};
+                        _data[_name] = _val;
+                        $.getJSON(_host, _data, function (_answear) {
+                            _cache[_name][_answear.input] = _answear.cities;
+                            if(_input.val() == _answear.input) {
+                                _done(_answear.cities);
+                            }
+                        });
+                    }
+                });
+            });
+            $(document).on('click', '#output a', function (e) {
+                var _contents = $(this).text();
+                var _space = _contents.indexOf(' ');
+                if(~_space) {
+                    _code.val(_contents.substr(0, _space));
+                    _city.val(_contents.substr(_space).trim());
+                    _out.empty();
+                }
+                e.preventDefault();
+                e.stopPropagation();
+                return false;
+            });
+        });
+    </script>
+
+    <script src="<?php echo BASE_URL?>assets/js/api.js" type="text/javascript"></script>
     </body>
 </html>
