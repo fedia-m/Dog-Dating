7b213cf986c0eb955ec4c582d44566357ca1734d
mahatehotia <mahatehotia@hotmail.com>
2018-05-24 14:46:35 +0200

    modif lien

--- config.php
+++ config.php
@@ -5,8 +5,8 @@ define("username","cesi");
 define("password",base64_decode("Y2VzaQ=="));
 
 
-//define("BASE_URL","http://localhost/Dog-Dating/");
-define("BASE_URL","http://192.168.43.242/Dog-Dating/");
+define("BASE_URL","http://localhost/Dog-Dating/");
+//define("BASE_URL","http://192.168.43.242/Dog-Dating/");
 
 
 define("default_controller","Accueil");
